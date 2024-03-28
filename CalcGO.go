bdb586b628fee33d9499531341e6250cd58485f2
Matheus Anitelli <mattanitelli4@gmail.com>
2024-03-28 18:48:44 -0300

    Correções de erros e aplicação do switch case

--- CalcGO.go
+++ CalcGO.go
@@ -17,12 +17,12 @@ func mult(a, b int) int {
 	return a * b
 }
 
-func div(a, b int) (int, error) {
+func div(a, b int) (float64, error) {
 	if b == 0 {
-		return 0, fmt.Errorf("Erro: Divisão por zero não é permitido")
-	} else {
-		return a / b, nil
+		return 0, fmt.Errorf("Erro: Divisão por zero não é permitida")
 	}
+	return float64(a) / float64(b), nil
+
 }
 
 func main() {
@@ -33,55 +33,56 @@ func main() {
 	)
 
 	for {
-
 		fmt.Println("\n~~~ Bem-vindo à Calculadora Simples em Go! ~~~")
-		fmt.Print("\n ==== MENU DE OPÇÕES ====\n[1] - Soma\n[2] - Subtração\n[3] - Multiplicação\n[4] - Divisão\n[0] - Sair\n")
+		fmt.Print("\n ===z= MENU DE OPÇÕES ====\n[1] - Soma\n[2] - Subtração\n[3] - Multiplicação\n[4] - Divisão\n[0] - Sair\n")
 
 		fmt.Print("\nEscolha qual opção deseja: ")
-		fmt.Scanf("%d", &opcao)
+		fmt.Scanln(&opcao)
 
 		switch opcao {
-
 		case 1:
-			fmt.Println("Digite o primeiro número: ")
-			fmt.Scanf("%d", &num1)
+			fmt.Print("Digite o primeiro número: ")
+			fmt.Scanln(&num1)
 
 			fmt.Print("Digite o segundo número: ")
-			fmt.Scanf("%d", &num2)
+			fmt.Scanln(&num2)
+
 			fmt.Printf("Resultado: %d\n", sum(num1, num2))
 
 		case 2:
-			fmt.Println("Digite o primeiro número: ")
-			fmt.Scanf("%d", &num1)
+			fmt.Print("Digite o primeiro número: ")
+			fmt.Scanln(&num1)
 
 			fmt.Print("Digite o segundo número: ")
-			fmt.Scanf("%d", &num2)
+			fmt.Scanln(&num2)
+
 			fmt.Printf("Resultado: %d\n", sub(num1, num2))
 
 		case 3:
-			fmt.Println("Digite o primeiro número: ")
-			fmt.Scanf("%d", &num1)
+			fmt.Print("Digite o primeiro número: ")
+			fmt.Scanln(&num1)
 
 			fmt.Print("Digite o segundo número: ")
-			fmt.Scanf("%d", &num2)
+			fmt.Scanln(&num2)
+
 			fmt.Printf("Resultado: %d\n", mult(num1, num2))
 
 		case 4:
-			fmt.Println("Digite o primeiro número: ")
-			fmt.Scanf("%d", &num1)
+			fmt.Print("Digite o primeiro número: ")
+			fmt.Scanln(&num1)
 
 			fmt.Print("Digite o segundo número: ")
-			fmt.Scanf("%d", &num2)
+			fmt.Scanln(&num2)
 
 			resultado, err := div(num1, num2)
 			if err != nil {
 				fmt.Println("Erro:", err)
 			} else {
-				fmt.Println("Resultado da Divisão:", resultado)
+				fmt.Printf("Resultado da Divisão: %.2f", resultado)
 			}
 
 		case 0:
-			fmt.Println("Obrigado por usar nossa calculadora !!")
+			fmt.Println("Obrigado por usar nossa calculadora!!")
 			os.Exit(0) // Saída normal
 
 		default:
