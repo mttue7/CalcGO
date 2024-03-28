0d0feb207091e3726114c14bf0efa023b08d9d28
Matheus Anitelli <mattanitelli4@gmail.com>
2024-03-28 18:04:44 -0300

    Correções de erro e saida do código

--- CalcGO.go
+++ CalcGO.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func sum(a, b int) int {
@@ -31,59 +32,61 @@ func main() {
 		num1, num2 int
 	)
 
-	fmt.Println("\n~~~ Bem-vindo à Calculadora Simples em Go! ~~~")
-	fmt.Print("\n ==== MENU DE OPÇÕES ====\n[1] - Soma\n[2] - Subtração\n[3] - Multiplicação\n[4] - Divisão\n[q] - Sair\n")
+	for {
 
-	fmt.Print("Escolha qual opção deseja: ")
-	fmt.Scanf("%d", &opcao)
+		fmt.Println("\n~~~ Bem-vindo à Calculadora Simples em Go! ~~~")
+		fmt.Print("\n ==== MENU DE OPÇÕES ====\n[1] - Soma\n[2] - Subtração\n[3] - Multiplicação\n[4] - Divisão\n[0] - Sair\n")
 
-	for num1 || num2 !='n'  {
+		fmt.Print("\nEscolha qual opção deseja: ")
+		fmt.Scanf("%d", &opcao)
+
 		switch opcao {
 
 		case 1:
-			fmt.Print("Digite o primeiro número: ")
-			fmt.Scanf("%d\n", &num1)
+			fmt.Println("Digite o primeiro número: ")
+			fmt.Scanf("%d", &num1)
 
 			fmt.Print("Digite o segundo número: ")
-			fmt.Scanf("%d\n", &num2)
+			fmt.Scanf("%d", &num2)
 			fmt.Printf("Resultado: %d\n", sum(num1, num2))
 
 		case 2:
-			fmt.Print("Digite o primeiro número: ")
-			fmt.Scanf("%d\n", &num1)
+			fmt.Println("Digite o primeiro número: ")
+			fmt.Scanf("%d", &num1)
 
 			fmt.Print("Digite o segundo número: ")
-			fmt.Scanf("%d\n", &num2)
+			fmt.Scanf("%d", &num2)
 			fmt.Printf("Resultado: %d\n", sub(num1, num2))
 
 		case 3:
-			fmt.Print("Digite o primeiro número: ")
-			fmt.Scanf("%d\n", &num1)
+			fmt.Println("Digite o primeiro número: ")
+			fmt.Scanf("%d", &num1)
 
 			fmt.Print("Digite o segundo número: ")
-			fmt.Scanf("%d\n", &num2)
+			fmt.Scanf("%d", &num2)
 			fmt.Printf("Resultado: %d\n", mult(num1, num2))
 
 		case 4:
-			fmt.Print("Digite o primeiro número: ")
+			fmt.Println("Digite o primeiro número: ")
 			fmt.Scanf("%d", &num1)
-		
+
 			fmt.Print("Digite o segundo número: ")
 			fmt.Scanf("%d", &num2)
-		
+
 			resultado, err := div(num1, num2)
 			if err != nil {
 				fmt.Println("Erro:", err)
 			} else {
 				fmt.Println("Resultado da Divisão:", resultado)
 			}
-		
-		case 'q':
-			fmt.Println("\nObrigado por usar nossa calculadora.")
-			return
+
+		case 0:
+			fmt.Println("Obrigado por usar nossa calculadora !!")
+			os.Exit(0) // Saída normal
 
 		default:
 			fmt.Println("Opção inválida!")
+			continue
 
 		}
 	}
